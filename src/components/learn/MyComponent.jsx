ae0c97fa18d0ba1b8523e53fc243e72806905601
Bui Cong Hieu <buihieu9259@gmail.com>
2025-02-08 16:42:41 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,18 @@
 import './style.css';
 
 const MyComponent = () => {
+  //biến số với jsx
+  // const hieu = "dep trai"; // string
+  // const hieu = "25"; // number
+  // const hieu = true; // boolean
+  // const hieu = null; // null
+  // const hieu = undefined; // undefined
+  const hieu = [1,2,3];
+  // const hieu = {name: "Bùi Hiếu",age : 21};
   return (
     <>
-      <h1>Hello world & Bùi Hiếu</h1>
+      <h1>Hello world & {JSON.stringify(hieu)} </h1> 
+      {/* cai nay dung de convert chuỗi và object sang string */}
       <div>Bùi Hiếu update</div>
       <div className="test">test</div>
     </>
