1e29eb2f4b6d230a5988fca095b7e4a6ef4d7f25
Bui Cong Hieu <buihieu9259@gmail.com>
2025-02-08 20:33:07 +0700

    video 33:  bai tap component

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData =() => {
+    return (
+        <div className="todo-data">
+            <div>Learning react</div>
+            <div>watch video</div>
+        </div>
+    )
+}
+
+export default TodoData;
