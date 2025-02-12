cdc2b846aa96edfce7062ac6e3a945d46b1fa448
Bui Cong Hieu <buihieu9259@gmail.com>
2025-02-12 21:02:02 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import TodoNew from './components/todo/TodoNew'
 import TodoData from './components/todo/TodoData'
 import reactLogo from './assets/react.svg'
 const App = () => {
-
+  //{key:value}
+  const buihieu = "hieu dep trai vai lua";
+  const age = 25;
+  const data ={
+    name: "Bùi Hiếu",
+    hobbies: ["reading", "painting", "cooking"],
+    address: {
+      street: "Nhan Trach, Ha Dong",
+      city: "hanoi",
+      country: "VIETNAM"
+    }
+  }
   return (
     <div className="Todo-Container">
       <div className="todo-title">Todo List</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData
+      name = {buihieu}
+      age = {age}
+      data = {data}
+      />
       <div className='todo-image'>
       <img src={reactLogo} className='logo' />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,11 @@
-const TodoData =() => {
+// props là properties và là 1 biến object khi muốn truy xuất dlieu can phai theo cú pháp : props.tenthuotinh
+// props là 12 object 
+const TodoData =(props) => {
+    // const {name,age,data}=props;
+    console.log(">> check props: ",props);
     return (
         <div className="todo-data">
+            <div>My name is {props.name}</div>
             <div>Learning react</div>
             <div>watch video</div>
         </div>
