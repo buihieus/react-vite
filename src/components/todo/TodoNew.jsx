8c4c363a7324da1bfa6bd7108f3a5bdb3d6ff0f1
Bui Cong Hieu <buihieu9259@gmail.com>
2025-02-12 22:56:21 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,28 @@
 const TodoNew =(props) => {
     console.log(">>> check point: ",props)
     const {addNewTodo} = props;
-    addNewTodo("Hieu");
-    return (
+    // addNewTodo("Hieu");
 
+    const handleClick = () => {
+        alert("click me");
+    }
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange",name);
+        // event.target là trỏ đến cái input ( dòng 14) .value là gtri của ô input
+    }
+    // const handleOnChange = (event) => {
+    //     console.log(">>> handleOnChange",event.target.value);
+    //     // event.target là trỏ đến cái input ( dòng 14) .value là gtri của ô input
+    // }
+    return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" 
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button style={
+                {cursor: "pointer"}}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
