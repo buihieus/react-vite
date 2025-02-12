d8f718e4bedeb9e98adb04876a6d2550ea73a661
Bui Cong Hieu <buihieu9259@gmail.com>
2025-02-12 21:22:02 +0700

    video34

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,11 +1,16 @@
 // props là properties và là 1 biến object khi muốn truy xuất dlieu can phai theo cú pháp : props.tenthuotinh
 // props là 12 object 
 const TodoData =(props) => {
-    // const {name,age,data}=props;
+    const {name,age,data}=props;
+    // hoặc viết như này cũng đc
+    //const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+    
     console.log(">> check props: ",props);
     return (
         <div className="todo-data">
-            <div>My name is {props.name}</div>
+            <div>My name is {name}</div>
             <div>Learning react</div>
             <div>watch video</div>
         </div>
