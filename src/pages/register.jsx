f300b2265b946e67ee889ad01d3d753d6373e62d
buihieus <buihieu9259@gmail.com>
2025-08-07 22:59:34 +0700

    video 89:hoan thien tinh nang register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,32 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, notification } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();// nghien cuu cai nay 
+     const navigate = useNavigate();
+    const onFinish = async (values) => {
+        // console.log("check values", values)
 
-    const onFinish =(values)=> {
-        console.log("check values",values)
+        // call api tu service api (registerUserAPI)
+        const res = await registerUserAPI(
+            values.fullName, 
+            values.email, 
+            values.password, 
+            values.phone);
+            if(res.data){
+                notification.success({
+                    message:"register users",
+                    description:"dang ky thanh cong"
+                });
+                // navigate("/login")
+                navigate("/")
+            }else{
+                notification.error({
+                    message:"register users error",
+                    description:JSON.stringify(res.message)
+                })
+            }
     }
     const onFinishFailed = () => {
 
@@ -15,8 +37,8 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
 
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
         >
             <div style={{
                 margin: "50px",
@@ -28,6 +50,7 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Full Name"
                         name="fullName"
+                        //rule nay dung de validate ben phia fontend
                         rules={[{ required: true, message: 'Please input your username!' }]}
                     >
                         <Input />
@@ -37,7 +60,7 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Email"
                         name="email"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                        rules={[{ required: true, message: 'Please input your email!' }]}
                     >
                         <Input />
                     </Form.Item>
@@ -46,7 +69,13 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Password"
                         name="password"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                        rules={[{ required: true, message: 'Please input your password!' },
+                        // use regex 
+                        // {
+                        //     pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/,
+                        //     message: 'Mật khẩu phải ≥6 ký tự, có chữ hoa, chữ thường, số và ký tự đặc biệt.'
+                        // }
+                        ]}
                     >
                         <Input.Password />
                     </Form.Item>
@@ -55,16 +84,21 @@ const RegisterPage = () => {
                     <Form.Item
                         label="Phone Numbe"
                         name="phone"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                        rules={[{
+                            // nay van loi chi ghi chu la sai chu + so lai dung =))
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }]}
                     >
-                        <Input />
-                    </Form.Item>
-                </div>
-                <div>
-                    <Button onClick={()=> form.submit()} type="primary">Register</Button>
-                </div>
+                    <Input />
+                </Form.Item>
             </div>
-        </Form>
+            <div>
+                <Button onClick={() => form.submit()} type="primary">Register</Button>
+            </div>
+        </div>
+        </Form >
     )
 }
 
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -60,11 +60,23 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
     }
     return axios.put(URL_BACKEND, data);
 }
+const registerUserAPI = (fullName, email, password, phone) => {
+    const URL_BACKEND = "/api/v1/user/register";
+    const data = {
+        // DINH DANG CUA NO LA 1 BIEN OBJECTS
+        fullName: fullName,
+        email: email,
+        password: password,
+        phone: phone
+    }
+    return axios.post(URL_BACKEND, data);
+}
 export {
     createUserAPI,
     updateUserAPI,
     fetchAllUserAPI,
     deleteUserAPI,
     handleUploadFile,
-    updateUserAvatarAPI
+    updateUserAvatarAPI,
+    registerUserAPI
 }
